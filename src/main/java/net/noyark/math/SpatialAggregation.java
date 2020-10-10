7a568a1a02ac34b474ce73cb0c186170c6d11068
magiclu550 <843983728@qq.com>
2020-10-10 10:16:33 +0800

    SpatialAggregation

--- src/main/java/net/noyark/math/SpatialAggregation.java
+++ src/main/java/net/noyark/math/SpatialAggregation.java
@@ -13,4 +13,19 @@ public interface SpatialAggregation extends SpatialElement{
     default boolean contains(Line line){
         return false;
     }
+
+    static SpatialAggregation define(Position p1,Position p2){
+        return new Line(p1,p2);
+    }
+    static SpatialAggregation define(Position p1,Position p2,Position p3){
+        return new Plane(p1,p2,p3);
+    }
+
+    static SpatialAggregation define(Position p1,Position p2,SRCS srcs){
+        return srcs.defineLine(p1,p2);
+    }
+    static SpatialAggregation define(Position p1,Position p2,Position p3,SRCS srcs){
+        return srcs.definePlane(p1,p2,p3);
+    }
+
 }
